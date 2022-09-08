2710d2b7c698a6b274fb4d0ccecd68eb2abf1e80
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2022-09-08 22:51:21 +0100

    Header section first part

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Scroll to Up
+
+let mySpan = document.querySelector("#scrollBttn");
+let test = true;
+
+if (test) {
+  window.onscroll = function () {
+    if (this.scrollY >= 600) {
+      mySpan.classList.add("show");
+    } else {
+      mySpan.classList.remove("show");
+    }
+    // another way to write this code
+    //   this.scrollY >= 600
+    //     ? mySpan.classList.add("show")
+    //     : mySpan.classList.remove("show");
+  };
+
+  mySpan.onclick = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+}
+// ---------------------------------------------------------
