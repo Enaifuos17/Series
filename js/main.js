d410bad47f712fe281b0fe2317c3972591b7efb8
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2022-10-01 01:51:11 +0100

    Upload the Languages Section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Scroll to Up
+// Scroll to Up (button)
 
 let mySpan = document.querySelector("#scrollBttn");
 let test = true;
@@ -26,4 +26,17 @@ if (test) {
 
 // ---------------------------------------------------------
 
-// go to types window
+// Animate Width On Scrolling (Languages Section)
+
+let languages_section = document.querySelector(".languages");
+let spans = document.querySelectorAll(".languages .progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= languages_section.offsetTop - 10) {
+    // console.log("DONE DONE");
+    spans.forEach((span) => {
+      // to take the width of the data-width attribute
+      span.style.width = span.dataset.width;
+    });
+  }
+};
